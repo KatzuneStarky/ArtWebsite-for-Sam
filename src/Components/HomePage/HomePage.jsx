eb1cea154e9a8d2784308b375c752f4f284f18f0
KatzuneStarky <aramirez_17@alu.uabcs.mx>
2022-03-04 15:58:22 -0700

    Cambios pequeños

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,17 @@
 import React , { useEffect } from 'react'
 import Slider from '../Slider/Slider'
 import './HomePage.css'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 function HomePage() {
   return (
     <>
-        <div className="homePage">
-          <Slider />        
+        <div className="homePage" style={{
+          background: `url('${process.env.PUBLIC_URL}/images/parallaximg2.jpg')`,
+          backgroundAttachment: 'fixed',
+          backgroundRepeat: 'no-repeat'
+          }}>        
+          <Slider />           
         </div>    
     </>
   )
