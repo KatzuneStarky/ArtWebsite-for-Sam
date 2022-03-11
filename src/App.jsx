137b10fdf6a2ad4247ce706a654bf0b4b6647fa8
KatzuneStarky <aramirez_17@alu.uabcs.mx>
2022-03-11 13:06:06 -0700

    Cambios finales

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
             <Route path='/blog' element={<AddArticle />} />
           </Routes>  
         </div>
+        <HomePage />
       </Router>            
     </>
   );
